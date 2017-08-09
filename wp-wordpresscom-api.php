22566d3fc6c95be184dc2913c37dee59a2aaec51
Brandon Hubbard <bkhubbard@gmail.com>
2017-08-09 11:34:01 -0700

    Started Project

---
+++ wp-wordpresscom-api.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Library for accessing the WordPress.com API on WordPress
+ *
+ * @link https://developer.wordpress.com/docs/ API Documentation
+ * @link https://developer.wordpress.com/docs/api/console/ Console
+ * @package WP-API-Libraries\WP-WordPressCom-API
+ */
+ 
+/*
+ * Plugin Name: WordPress.com API
+ * Plugin URI: https://wp-api-libraries.com/
+ * Description: Perform API requests.
+ * Author: WP API Libraries
+ * Version: 1.0.0
+ * Author URI: https://wp-api-libraries.com
+ * GitHub Plugin URI: https://github.com/wp-api-libraries/wp-wordpresscom-api
+ * GitHub Branch: master
+ */
+ 
+/* Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+if ( ! class_exists( 'WordPressComAPI' ) ) {
+	
+	class WordPressComAPI {
+		
+	}
+	
+}
+
