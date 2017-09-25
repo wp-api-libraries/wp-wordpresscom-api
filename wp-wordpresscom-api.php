66f4480c8af975562d47752bc2584fe99305f1b2
Santiago Garza <santiago.garza@imforza.com>
2017-09-25 12:04:31 -0700

    Started filling out wpcom api

--- wp-wordpresscom-api.php
+++ wp-wordpresscom-api.php
@@ -6,7 +6,7 @@
  * @link https://developer.wordpress.com/docs/api/console/ Console
  * @package WP-API-Libraries\WP-WordPressCom-API
  */
- 
+
 /*
  * Plugin Name: WordPress.com API
  * Plugin URI: https://wp-api-libraries.com/
@@ -17,15 +17,139 @@
  * GitHub Plugin URI: https://github.com/wp-api-libraries/wp-wordpresscom-api
  * GitHub Branch: master
  */
- 
+
 /* Exit if accessed directly */
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 if ( ! class_exists( 'WordPressComAPI' ) ) {
-	
-	class WordPressComAPI {
-		
-	}
-	
-}
 
+	class WordPressComAPI {
+
+		/**
+		 * API Key.
+		 *
+		 * @var string
+		 */
+		static protected $oauth_token;
+
+		/**
+		 * CloudFlare BaseAPI Endpoint
+		 *
+		 * @var string
+		 * @access protected
+		 */
+		protected $base_uri = 'https://public-api.wordpress.com/rest/v1.1/';
+
+		/**
+		 * Route being called.
+		 *
+		 * @var string
+		 */
+		protected $route = '';
+
+
+		/**
+		 * Class constructor.
+		 *
+		 * @param string $oauth_token  Wordpress.com oauth Key.
+		 */
+		public function __construct( $oauth_token ) {
+			static::$oauth_token = trim( $oauth_token );
+		}
+
+		/**
+		 * Prepares API request.
+		 *
+		 * @param  string $route   API route to make the call to.
+		 * @param  array  $args    Arguments to pass into the API call.
+		 * @param  array  $method  HTTP Method to use for request.
+		 * @return self            Returns an instance of itself so it can be chained to the fetch method.
+		 */
+		protected function build_request( $route, $args = array(), $method = 'GET' ) {
+			// Start building query.
+			$this->set_headers();
+			$this->args['method'] = $method;
+			$this->route = $route;
+
+			// Generate query string for GET requests.
+			if ( 'GET' === $method ) {
+				$this->route = add_query_arg( array_filter( $args ), $route );
+			} elseif ( 'application/json' === $this->args['headers']['Content-Type'] ) {
+				$this->args['body'] = wp_json_encode( $args );
+			} else {
+				$this->args['body'] = $args;
+			}
+
+			return $this;
+		}
+
+
+		/**
+		 * Fetch the request from the API.
+		 *
+		 * @access private
+		 * @return array|WP_Error Request results or WP_Error on request failure.
+		 */
+		protected function fetch() {
+			// Make the request.
+			$response = wp_remote_request( $this->base_uri . $this->route, $this->args );
+
+			// Retrieve Status code & body.
+			$code = wp_remote_retrieve_response_code( $response );
+			$body = json_decode( wp_remote_retrieve_body( $response ) );
+
+			$this->clear();
+			// Return WP_Error if request is not successful.
+			if ( ! $this->is_status_ok( $code ) ) {
+				return new WP_Error( 'response-error', sprintf( __( 'Status: %d', 'wp-postmark-api' ), $code ), $body );
+			}
+
+			return $body;
+		}
+
+
+		/**
+		 * Set request headers.
+		 */
+		protected function set_headers() {
+			// Set request headers.
+			$this->args['headers'] = array(
+				  'Authorization' => 'Bearer '. static::$oauth_token,
+					'Content-Type' => 'application/json',
+			);
+		}
+
+		/**
+		 * Clear query data.
+		 */
+		protected function clear() {
+			$this->args = array();
+			$this->query_args = array();
+		}
+
+		/**
+		 * Check if HTTP status code is a success.
+		 *
+		 * @param  int $code HTTP status code.
+		 * @return boolean       True if status is within valid range.
+		 */
+		protected function is_status_ok( $code ) {
+			return ( 200 <= $code && 300 > $code );
+		}
+
+		/**
+		 * Get User Properties
+		 *
+		 * Account Access: FREE, PRO, Business, Enterprise
+		 *
+		 * @api GET
+		 * @see https://api.cloudflare.com/#user-user-details Documentation.
+		 * @access public
+		 * @return array  User information.
+		 */
+		public function get_user() {
+			return $this->build_request( 'user' )->fetch();
+		}
+	}
+
+}
